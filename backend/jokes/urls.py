afc41d1c9d1bfc127561d3c711313ff23e8e4c5c
Azeem Shaikh <azeem6755@gmail.com>
2020-11-03 09:47:01 +0530

    Remove backend

--- backend/jokes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import get_joke
-
-urlpatterns = [
-    path('', get_joke, name='index'),
-]
--- backend/jokes/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
-
-# Create your views here.
-def get_joke(request):
-    print('Settings', settings.JOKE_APIS.get('get_joke'))
-    return HttpResponse("Hello, world. You're at the polls index.")
