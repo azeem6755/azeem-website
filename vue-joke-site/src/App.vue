017541906025c0cbf2a6e0a70ae99a3a792d1cb4
Azeem Shaikh <azeem6755@gmail.com>
2020-11-06 23:31:36 +0530

    Add First page

--- vue-joke-site/src/App.vue
+++ vue-joke-site/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <front-page title="Custom Component"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FrontPage from './components/FrontPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FrontPage
   }
 }
 </script>
---
+++ vue-joke-site/src/components/FrontPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="title">
+        {{title}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FrontPage',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style scoped>
+    .title{
+        font-size: 18px;
+        color: green;
+    }
+</style>
